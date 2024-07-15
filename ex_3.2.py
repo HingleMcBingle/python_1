03288131c6ab21506ba5ae796315e4d34b6af315
Sterling <dorseyclique+git@gmail.com>
2024-07-15 17:17:52 -0500

    correct unecessary if statement

--- ex_3.2.py
+++ ex_3.2.py
@@ -1,10 +1,11 @@
-hrs =  input("How Many Hours Worked?")
+hrs =  input("How Many Hours Worked?\n")
 hrs = float (hrs)
 if hrs > 40 :
         hrs_plus = (hrs - 40)
-if hrs_plus > 0 :
-       hrs_reg = (hrs - hrs_plus)
-rate =  input("Whats the Hourly Rate?")
+if hrs <= 40 :
+        hrs_plus = 0
+hrs_reg = (hrs - hrs_plus)
+rate =  input("Whats the Hourly Rate?\n")
 rate = float(rate)
 rate_plus = (rate * 1.5)
 print(hrs_reg * rate + hrs_plus * rate_plus)
